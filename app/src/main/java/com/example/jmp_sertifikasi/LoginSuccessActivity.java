2a835edc7ade4a150fde2c15ebe539ecd5f984b7
lailatulbadriyah24 <lailatulbadriyah654@gmail.com>
2023-08-15 19:29:24 +0700

    Fixing SplashScreen, Login

--- app/src/main/java/com/example/jmp_sertifikasi/LoginSuccessActivity.java
+++ app/src/main/java/com/example/jmp_sertifikasi/LoginSuccessActivity.java
@@ -2,13 +2,22 @@ package com.example.jmp_sertifikasi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class LoginSuccessActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_success);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
+                finish();
+            }
+        }, 5000L); //5000 L = 5 detik
     }
 }
